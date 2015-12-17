365d330c558093c81a0a4d37251951c857b82b2d
michael.keetman <mkeetman@gmail.com>
2015-12-17 16:58:18 +1000

    Started implementing the coach view

--- Eudamonia/dashboard/admin.py
+++ Eudamonia/dashboard/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Athlete
+from .models import Athlete, Coach
 
 
 # Register your models here.
 admin.site.register(Athlete)
+admin.site.register(Coach)
